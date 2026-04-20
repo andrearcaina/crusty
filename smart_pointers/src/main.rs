526751a6f97eed0f8ae22270b2394c937a98a75d
Andre Arcaina <dtandre331@gmail.com>
2026-04-20 14:36:21 -0400

    Rc and smart pointers done
    fearless concurrency next (16)

--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -1,8 +1,44 @@
 use crate::List::{Cons, Nil};
+use crate::ListRc::{Cons as ConsRc, Nil as NilRc};
+use std::ops::Deref;
+use std::rc::Rc;
 
 enum List {
     Cons(i32, Box<List>),
     Nil,
+}
+
+enum ListRc {
+    Cons(i32, Rc<ListRc>),
+    Nil,
+}
+
+// defining our own smart pointer
+#[derive(Debug)]
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> Self {
+        Self(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
+    }
 }
 
 fn main() {
@@ -32,4 +68,62 @@ fn main() {
     }
 
     print_list(&list);
+
+    // if we try to assert the value of a box, we get a compile-time error:
+    // assert_eq!(b, 5); will not compile
+    //
+    // this is because we cannot dereference a box directly - we need to use the `*` operator to get the value inside the box
+    assert_eq!(*b, 5); // if nothing prints and program runs, the assertion passes and the program continues
+
+    let x = 5;
+    let y = Box::new(x);
+
+    assert_eq!(x, 5); // check if the value of x is 5
+    assert_eq!(*y, 5); // check if the value inside the box is 5
+
+    let z = MyBox::new(x);
+    println!("{:?}", z);
+    assert_eq!(*z, 5); // check if the value inside the custom box is 5 (works because we implemented Deref for MyBox)
+
+    let m = MyBox::new(String::from("Rust"));
+    hello(&m); // deref coercion: &MyBox<String> -> &str
+    hello(&*m); // explicit deref to String, then coerced to &str
+    hello(&(*m)[..]); // explicit deref + slicing to &str
+
+    let c = CustomSmartPointer {
+        data: String::from("my stuff"),
+    };
+    let d = CustomSmartPointer {
+        data: String::from("other stuff"),
+    };
+    println!("CustomSmartPointers created");
+
+    // c and d are dropped when they go out of scope
+    // can't manually drop a box or custom smart pointer
+    // c.drop() won't compile
+    // drop(c); can be used to drop a box or custom smart pointer, but it's not recommended
+    //
+    drop(c);
+    drop(d);
+
+    println!("CustomSmartPointers dropped before the end of main");
+
+    // sharing data across smart pointers using Rc (reference counted smart pointer)
+    // basically a Box is a smart pointer that stores data on the heap and is dropped when it goes out of scope
+    // Rc is similar, but it keeps track of how many references to the data exist and only drops the data when there are no more references
+    // Rc also shares ownership of the data, so multiple Rc pointers can point to the same data
+
+    let a = Rc::new(ConsRc(5, Rc::new(ConsRc(10, Rc::new(NilRc)))));
+    println!("count after creating a = {}", Rc::strong_count(&a));
+    let b = ConsRc(3, Rc::clone(&a)); // basically b shares ownership of the data with a and theres a total of 2 references (with Rc)
+    println!("count after creating b = {}", Rc::strong_count(&a));
+    {
+        let c = ConsRc(4, Rc::clone(&a)); // c shares ownership of the data with a and theres a total of 3 references (with Rc)
+        println!("count after creating c = {}", Rc::strong_count(&a));
+    }
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a)); // once c goes out of scope, the total references drop to 2
+}
+
+fn hello(name: &str) {
+    println!("Hello, {}!", name);
 }
